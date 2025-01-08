515940e81fbb32ec43a494c3b73f9e9bcca88889
Jathin <jathin317@gmail.com>
2025-01-08 22:26:41 +0530

    Add programs for divisibility check by 3 and 5, and number pattern generation

---
+++ div3and5.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class div3and5{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        if(number%3==0 && number%5==0)
+        {
+            System.out.println("Number is divisible by 3 and 5");
+        }
+        else
+        {
+            System.out.println("Number is not divisible by 3 and 5");
+        }
+        sc.close();
+    }
+}
