7919d1b998ffbd070575b6a8dea3101985121f2c
Jathin <jathin317@gmail.com>
2025-01-08 22:14:35 +0530

    renamed numbers to even numbers

--- even_numbers.java
+++ even_numbers.java
@@ -1,4 +1,4 @@
-public class numbers {
+public class even_numbers {
     public static void main(String[] args) {
         for (int i = 1; i <= 50; i++) {
             if (i % 5 != 0 && i % 2 == 0) {
